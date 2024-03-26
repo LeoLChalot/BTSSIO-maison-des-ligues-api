22c5e7952bfab976b0149ff48b5624558bda6649
LeoLChalot <77191750+LeoLChalot@users.noreply.github.com>
2024-03-26 11:32:38 +0100

    update server.js

--- api/server.js
+++ api/server.js
@@ -22,11 +22,18 @@ app.use(
  );
 
 
-<<<<<<< HEAD
-const whiteList = ['http://localhost:3000', 'http://192.168.1.35:3000/','http://localhost:5173', 'http://localhost:50433', 'http://localhost:45820'];
-=======
-const whiteList = ['http://localhost:3000', 'http://localhost:5173', 'http://192.168.1.35:3000/'];
->>>>>>> 24f878a29e8e6c18469eb33b17c7eb9e76f18119
+
+const whiteList = [
+	'http://localhost:3000',
+	'http://127.0.0.1:5173',
+	'http://192.168.58.199:5173',
+	'http://192.168.1.35:3000',
+	'http://192.168.1.34:5173',
+	'http://localhost:5173',
+	'http://localhost:50433',
+	'http://localhost:45820'];
+
+
 const corsOptions = {
    origin: function (origin, callback) {
       if (whiteList.indexOf(origin) !== -1 || !origin) {
@@ -59,30 +66,6 @@ app.use('/m2l/admin', routesAdmin);
 
 // ? Router Panier
 app.use('/m2l/panier', routesPanier);
-
-// ? Router Tests
-app.use('/m2l/tests', routesTests);
-
-// ? Router Error 400
-app.use((req, res, next) => {
-   res.status(400).json({ message: 'Bad request' });
-});
-
-// ? Router Error 401
-app.use((req, res, next) => {
-   res.status(401).json({ message: 'Unauthorized' });
-});
-
-// ? Router Error 404
-app.use((req, res) => {
-   res.status(404).json({ message: 'Page not found' });
-});
-
-// ? Router Error 500
-app.use((err, req, res, next) => {
-   console.error(err.stack);
-   res.status(500).json({ message: 'Internal server error' });
-});
 
 module.exports = app.listen(PORT, () => {
    console.log(`Listen on port ${PORT}`);
